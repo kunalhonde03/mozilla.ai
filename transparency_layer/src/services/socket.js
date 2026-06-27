db4371955310e0b2503bf3415b22a248a707bef9
sahilgodage05-debug <sahilgodage05@gmail.com>
2026-06-27 18:19:45 +0530

    Fix duplicate declaration of currentMock causing Javascript crash on simulation interval

--- transparency_layer/src/services/socket.js
+++ transparency_layer/src/services/socket.js
@@ -190,7 +190,6 @@ class TelemetrySocketService {
       });
 
       // 3. Emit log event
-      const currentMock = mockPrompts[index];
       const now = new Date();
       const timestampString = `[${now.getFullYear()}-${String(now.getMonth()+1).padStart(2,'0')}-${String(now.getDate()).padStart(2,'0')} ${String(now.getHours()).padStart(2,'0')}:${String(now.getMinutes()).padStart(2,'0')}:${String(now.getSeconds()).padStart(2,'0')}]`;
 
